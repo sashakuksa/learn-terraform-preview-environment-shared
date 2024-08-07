6d3f25d5a5a1c628bb64d7994758052b04f24166
sashakuksa <63709299+sashakuksa@users.noreply.github.com>
2024-08-07 16:51:16 +0300

    Update main.tf

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -33,4 +33,17 @@ data "template_file" "user_data" {
   template = file("${path.module}/setup-hashicups.yaml")
 }
 
+resource "aws_instance" "hashicups-backend" {
+  count                       = var.is_prod ? 0 : 1
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = data.terraform_remote_state.shared.outputs.public_subnets[0]
+  vpc_security_group_ids      = [data.terraform_remote_state.shared.outputs.hashicups_security_group_id]
+  associate_public_ip_address = true
+  user_data                   = data.template_file.user_data.rendered
+
+  tags = {
+    Name = terraform.workspace
+  }
+}
 
